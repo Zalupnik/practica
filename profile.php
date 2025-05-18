ee97b8c396fb788ed9ae43045cf4180d177b0a3c
Саша <kxsvx@gmail.com>
2025-05-18 22:29:33 +0300

    fff

---
+++ profile.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Аккаунт</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <nav>
+        <div class="nav_block--one">
+            <div class="container navbar">
+                <div class="nav_moscow">
+                    <div class="moscow">Москва</div>
+                </div>
+                <div class="nav_link">
+                    <a href="delivery.html">Доставка</a>
+                    <a href="refund.html">Возврат</a>
+                    <a href="docs.html">Документация</a>
+                    <a href="contact.html">Контакты</a>
+                </div>
+            </div>
+        </div>
+        <div class="container nav_block">
+            <div class="nav_block--two">
+                <a href="index.html" class="block_logo_name">
+                    <img src="img/Light.png">
+                </a>
+                <a href="#" class="catalog_h">
+                    <div class="catalog_block">
+                        <img src="img/Icon.svg">
+                    </div>Каталог
+                </a>
+                <div class="nav_link">
+                    <a href="profile.php">
+                        <div class="nav_link_block">
+                            <img src="img/Icon (2).png" alt="profile">Профиль
+                        </div>
+                    </a>
+
+                    <a href="#">
+                        <div class="nav_link_block">
+                            <img src="img/Icon (3).png" alt="profile">Заказы
+                        </div>
+                    </a>
+
+                    <a href="#">
+                        <div class="nav_link_block">
+                            <img src="img/Icon (4).png" alt="profile">Корзина
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="nav_block--three">
+                <div class="nav_link">
+                    <a href="#">Акции</a>
+                    <a href="#">Строительные материалы</a>
+                    <a href="#">Керамическая плитка</a>
+                    <a href="#">Краски</a>
+                    <a href="#">Сантехника</a>
+                    <a href="#">Напольные покрытия</a>
+                    <a href="#">Инструменты</a>
+                    <a href="#">Обои</a>
+                    <a href="#">Окна</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section class="section_account">
+        <div class="container account">
+            <div class="account_content">
+                <div class="pp">
+                    <h1 class="h1 h1_delivery">Личный кабинет</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section_info">
+        <div class="container account">
+            <div class="container_contact_block">
+                <div class="account_info">
+                    <h1 class="h1_info">Личная информация</h1>
+                </div>
+                <div class="info_block-p">
+                    <div class="info_block_p-content one_cont">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
