1dab9a572920ebb7f38891725dfd4bed00ed8d9b
Саша <kxsvx@gmail.com>
2025-06-03 05:55:57 +0300

    Чо та

---
+++ admin.php
@@ -0,0 +1,20 @@
+
+
+<?include('header.php')?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Админ панель</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <section class="section_admin">
+        <div class="container admin">
+            
+        </div>
+    </section>
+</body>
+</html>
